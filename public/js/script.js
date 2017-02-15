100c497ef77e75e2d042d38e7f34356853120093
Jinn Wang <jinn.r.w@gmail.com>
2017-02-15 15:13:09 -0800

    update loading effect on Search

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,25 @@
 //var transition= document.getElementsByClassName('grayloader');
-        setTimeout(function() {
-        	$('.grayloader .imagesfake').css('opacity', '0');
-         
-	            $('.grayloader .imagesfake').css('display', 'none');
-	            
-	            $('.grayloader .colors, .grayloader h4, .grayloader p, .grayloader a, .grayloader .search-details p').css('background-color', 'transparent');
+setTimeout(function() {
+	$('.grayloader .imagesfake').css('opacity', '0');
+	
+		$('.grayloader .imagesfake').css('display', 'none');
+		
 
-	            $('.grayloader h4, .grayloader p').css('color', 'inherit');
-				$('.grayloader a').css('color', '#007bda');
-				$('.grayloader .search-tile').css('border-radius', '0');
+		setTimeout(function() {
+			$('.grayloader h4, .grayloader p').css('color', 'inherit');
+			$('.grayloader a').css('color', '#007bda');	
+		}, 300);
+		
+		$('.grayloader .colors, .grayloader h4, .grayloader p, .grayloader a, .grayloader .search-details p').css('background-color', 'transparent');
 
-	            $('.grayloader img').css('opacity', '1');
-	            $('.grayloader .shadow1').removeClass('shadow1');
-				$('.grayloader .shadow2').removeClass('shadow2');
-	            $('.grayloader div[class^="colors-"]').css('opacity', '1');
-	            $('.grayloader .grayline').css('opacity', '1');
+		// $('.grayloader .search-tile').css('border-radius', '0');
 
-				$('.grayloader h4, .grayloader p, .grayloader a, .grayloader .search-details p').css('user-select', 'auto');
-				
-        }, 4800);
+		$('.grayloader img').css('opacity', '1');
+		$('.grayloader .shadow1').removeClass('shadow1');
+		$('.grayloader .shadow2').removeClass('shadow2');
+		$('.grayloader div[class^="colors-"]').css('opacity', '1');
+		$('.grayloader .grayline').css('opacity', '1');
+
+		$('.grayloader h4, .grayloader p, .grayloader a, .grayloader .search-details p').css('user-select', 'auto');
+		
+}, 4800);
